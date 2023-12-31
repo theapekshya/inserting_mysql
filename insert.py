b796527a14b0953ee011a7f84cdf467f0c664e55
theapekshya <98611497+thealizstha@users.noreply.github.com>
2023-12-31 19:37:03 +0545

    inserting_mysql

---
+++ insert.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myusername",
+  password="mypassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
